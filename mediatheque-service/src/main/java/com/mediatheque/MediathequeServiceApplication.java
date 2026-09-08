379044213c9d23137709c48525199680d497a4a0
Fanto974 <mathis.buclon@efrei.net>
2026-09-08 23:22:56 +0200

    modify the project's structure to accomodate a multiple services app

---
+++ mediatheque-service/src/main/java/com/mediatheque/MediathequeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mediatheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediathequeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediathequeServiceApplication.class, args);
+	}
+
+}
--- user-service/src/main/java/com/users/UserServiceApplication.java
+++ user-service/src/main/java/com/users/UserServiceApplication.java
@@ -1,13 +1,13 @@
-package com.mediatheque;
+package com.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MediatechApplication {
+public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MediatechApplication.class, args);
+		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
 }
